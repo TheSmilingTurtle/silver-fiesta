2480a03154148cd88930e3eadb58d4ecc059a59b
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-23 17:06:57 +0100

    silver-fiesta

---
+++ silver-fiesta.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define silver_fiesta   int
+#define Silver_fiesta   main
+#define silveR_Fiesta   std
+#define silver_Fiesta   cout
+#define Silver_Fiesta   endl
+
+silver_fiesta Silver_fiesta(){
+    silveR_Fiesta::silver_Fiesta <<  "silver-fiesta" << silveR_Fiesta::Silver_Fiesta;
+}
